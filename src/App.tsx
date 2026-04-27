bf6809fc21d6f6dfc7ae4d0d5f39b15d77ee0114
E-genius <e-genius@Mac-mini-E-genius.local>
2026-04-27 16:30:40 +0300

    react dynamic list of goods - working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[] | null>(null);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const fetchGoods = async (fetcher: () => Promise<Good[]>) => {
+    try {
+      setGoods(null);
+      setIsLoading(true);
+      setError('');
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      const data = await fetcher();
+      setGoods(data);
+    } catch {
+      setError('Failed to load goods');
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => fetchGoods(getAll)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => fetchGoods(get5First)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => fetchGoods(getRedGoods)}
+      >
+        Load red goods
+      </button>
+
+      {error && <p>{error}</p>}
+
+      <GoodsList goods={goods} isLoading={isLoading} />
+    </div>
+  );
+};
--- src/GoodsList.tsx
+++ src/GoodsList.tsx
@@ -2,15 +2,32 @@ import React from 'react';
 import { Good } from './types/Good';
 
 type Props = {
-  goods: Good[];
+  goods: Good[] | null;
+  isLoading: boolean;
 };
 
-export const GoodsList: React.FC<Props> = ({ goods }) => (
-  <ul>
-    {goods.map(good => (
-      <li key={good.id} data-cy="good">
-        {good.name}
-      </li>
-    ))}
-  </ul>
-);
+export const GoodsList = React.memo(function GoodsList({
+  goods,
+  isLoading,
+}: Props) {
+  return (
+    <>
+      {isLoading && (
+        <div className="is-flex is-justify-content-start my-4">
+          <span
+            className="loader goods-loader"
+            aria-label="Loading goods"
+          ></span>
+        </div>
+      )}
+
+      <ul>
+        {goods?.map(good => (
+          <li key={good.id} data-cy="good" style={{ color: good.color }}>
+            {good.name}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+});
